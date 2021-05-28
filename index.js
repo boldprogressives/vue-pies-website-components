11bc786f2e7edd5cf46c11c406ef06cfa1b83adf
Ethan Jucovy <ethan.jucovy@gmail.com>
2021-05-28 10:51:48 -0400

    wip

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+import PButtons from './src/PButtons.vue';
+import PForm from './src/PForm.vue';
+import Logo from './src/Logo.vue';
+import Tile from './src/Tile.vue';
+import MediaUnit from './src/MediaUnit.vue';
+import TileRow from './src/TileRow.vue';
+
 export default {
-  PButtons: 'foo',
+  PButtons,
+  PForm,
+  Logo,
+  Tile,
+  MediaUnit,
+  TileRow,
 };
 
