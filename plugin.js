2f9bb6bf9168cc9727415e80299b791db440adc2
Ethan Jucovy <ethan.jucovy@gmail.com>
2021-05-28 11:21:30 -0400

    wip

---
+++ plugin.js
@@ -0,0 +1,15 @@
+import PButtons from './src/PButtons.vue';
+import PForm from './src/PForm.vue';
+import Tile from './src/Tile.vue';
+import MediaUnit from './src/MediaUnit.vue';
+import TileRow from './src/TileRow.vue';
+
+export default {
+  install(Vue, options) {
+    Vue.component('tile-row', TileRow);
+    Vue.component('tile', Tile);
+    Vue.component('media-unit', MediaUnit);
+    Vue.component('p-form', PForm);
+    Vue.component('p-buttons', PButtons);
+  }
+}
