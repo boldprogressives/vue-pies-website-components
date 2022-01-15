5e9274176108e20f10fac252f5f3a6d5a6b167b6
Ethan Jucovy <ethan.jucovy@gmail.com>
2022-01-15 14:35:40 -0500

    simplify feed config matching per page

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
-async function fetchFeeds ({ $content, feedConfig, pageTree }) {
+async function fetchFeeds ({ $content, feedConfig, pageTree, page }) {
   let allFeeds = {};
   for (let key of Object.keys(feedConfig)) {
     const conf = feedConfig[key];
-    allFeeds[key] = $content(conf.dir).sortBy(conf.sortBy).where(conf.where).limit(conf.limit);
+    if (conf.page === page) {
+      allFeeds[key] = $content(conf.dir).sortBy(conf.sortBy).where(conf.where).limit(conf.limit);
+    }
   }
 
-  const myFeedKeys = Object.keys(allFeeds).filter(key => pageTree[key]);
-  const myFeedValues = await Promise.all(Object.entries(allFeeds).filter(o => pageTree[o[0]]).map(o => o[1].fetch()));
+  const myFeedKeys = Object.keys(allFeeds);
+  const myFeedValues = await Promise.all(Object.entries(allFeeds).map(o => o[1].fetch()));
   
   let myFeeds = {};
   for (var i = 0; i < myFeedKeys.length; ++i) {
